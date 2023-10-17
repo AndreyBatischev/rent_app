ce4fca969c7ccfc1127113b138839be99e14805c
Andrey B <a.e.batischev@gmail.com>
2023-10-17 18:47:48 +0300

    create handel error func

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from '../models/userModel.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
 
-export const signUp = async(req, res) => {
+export const signUp = async(req, res, next) => {
    const {username, email, password} = req.body
    const hashedPassword = bcryptjs.hashSync(password, 10)
    const newUser = new User({username, email, password: hashedPassword})
@@ -9,7 +10,7 @@ export const signUp = async(req, res) => {
    await newUser.save()
    res.status(201).json('User created successfully')
   } catch (error) {
-   res.status(500).send(error)
+    next(error)
    
   }
 }
--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ app.use(express.json())
 app.use('/api/user',  userRoute)
 app.use('/api/auth',  authRoute)
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
+
