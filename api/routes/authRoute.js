28219c67799cf44931a91a0d842b4e689b9deb6d
Andrey B <a.e.batischev@gmail.com>
2023-10-18 21:01:25 +0300

    complite page sign in, api + ui

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signUp } from '../controllers/authController.js'
+import { signUp, signIn } from '../controllers/authController.js'
 
 const router = express.Router()
 
 router.post('/signup', signUp)
+router.post('/signin', signIn)
 
 export default router
