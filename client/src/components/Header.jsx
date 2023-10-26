c1824fb21db4f4a588fb03aca217b03a9fb121ae
Andrey B <a.e.batischev@gmail.com>
2023-10-26 20:48:37 +0300

    new logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,8 @@ export default function Header() {
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
 				<Link to="/">
 					<h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-						<span className="text-slate-500">You</span>
+						<span className="text-slate-700">You</span>
+						<span className="text-slate-500">Are</span>
 						<span className="text-slate-700">Rent</span>
 					</h1>
 				</Link>
